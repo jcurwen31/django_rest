423369bc4932f630c909d1653e6d6561b1bfbba1
joseph curwen <jcurwen31@gmail.com>
2019-10-11 22:51:05 +0200

    use model.serializer

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, LEXERS, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.Serializer):
+"""class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
@@ -21,4 +21,10 @@ class SnippetSerializer(serializers.Serializer):
         instance.style = validated_data.get('style', instance.style)
         instance.save()
         return instance
+"""
 
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
